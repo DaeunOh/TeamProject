7ad3baba80f088403055998d47bdf1d616ad5ad3
DaeunOh <d30902430@gmail.com>
2018-05-18 00:36:18 +0900

    기본 인터페이스

--- app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
+++ app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bbbb.teamproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost host = (TabHost)findViewById(R.id.host);
+        host.setup();
+
+        TabHost.TabSpec spec = host.newTabSpec("tab1");
+        spec.setContent(R.id.tab_content1);
+        spec.setIndicator("Tab 1");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab2");
+        spec.setContent(R.id.tab_content2);
+        spec.setIndicator("Tab 2");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab3");
+        spec.setContent(R.id.tab_content3);
+        spec.setIndicator("Tab 3");
+        host.addTab(spec);
+
+        spec = host.newTabSpec("tab4");
+        spec.setContent(R.id.tab_content4);
+        spec.setIndicator("Tab 4");
+        host.addTab(spec);
+
+
+
+
     }
 }
