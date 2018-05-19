c461fcc6510a78d468666da55887111dc6bb23dc
soominsun <soominsun@gmail.com>
2018-05-20 01:25:14 +0900

    다시 pull

--- app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
+++ app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.bbbb.teamproject;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +27,19 @@ public class MainActivity extends AppCompatActivity {
 
         spec = host.newTabSpec("tab2");
         spec.setContent(R.id.tab_content2);
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            Fragment2_1 firstFragment = new Fragment2_1();
+
+            getFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+
         spec.setIndicator("모아보기");
         host.addTab(spec);
 
@@ -37,5 +54,27 @@ public class MainActivity extends AppCompatActivity {
         host.addTab(spec);
 
 
+
     }
+
+    public void selectFragment(View view){
+        Fragment fr=null;
+
+        switch (view.getId()){
+            case R.id. button_map:
+                fr=new Fragment2_1();
+                break;
+            case R.id.button_sort:
+                fr=new Fragment2_2();
+                break;
+        }
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fr);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+
 }
