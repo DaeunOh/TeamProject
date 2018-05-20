0edaeeef74a412b84ee50f942538ec0be335cf75
DaeunOh <d30902430@gmail.com>
2018-05-21 03:19:16 +0900

    1. dpi에 맞게 아이콘 수정 2. 지도/종류 눌릴 때마다 버튼 색깔 스위치 3. 리뷰 등록 버튼 눌렀을 때 상황에 맞게 대화상자 띄우기

--- app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
+++ app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
@@ -7,19 +7,20 @@ import android.app.FragmentTransaction;
 import android.app.SearchManager;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.Toast;
 
@@ -28,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private DrawerLayout dlDrawer;
     private ActionBarDrawerToggle dtToggle;
+    private Button mapButton;
+    private Button sortButton;
     private android.support.v7.widget.SearchView searchView;
 
     @Override
@@ -35,10 +38,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         TabHost host = (TabHost) findViewById(R.id.host);
-        ImageButton rouletteButton = findViewById(R.id.button_roulette);
+        ImageButton rouletteButton = (ImageButton) findViewById(R.id.button_roulette);
+        mapButton = (Button) findViewById(R.id.button_map);
+        sortButton = (Button) findViewById(R.id.button_sort);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        searchView = findViewById(R.id.menu_search);
+        Button reviewButton = (Button) findViewById(R.id.button_review);
+        final EditText reviewText = findViewById(R.id.review_text);
+        final AutoCompleteTextView searchText = findViewById(R.id.search_text);
 
         new Roulette(MainActivity.this).setComponents(rouletteButton);
 
@@ -46,17 +54,15 @@ public class MainActivity extends AppCompatActivity {
 
         TabHost.TabSpec spec = host.newTabSpec("tab1");
         spec.setContent(R.id.tab_content1);
-        //spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_home, null));
-        spec.setIndicator("홈");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_home, null));
         host.addTab(spec);
 
         spec = host.newTabSpec("tab2");
         spec.setContent(R.id.tab_content2);
-        spec.setIndicator("모아보기");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_search, null));
         host.addTab(spec);
 
         if (findViewById(R.id.fragment_container) != null) {
-
             if (savedInstanceState != null) {
                 return;
             }
@@ -67,24 +73,46 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, firstFragment).commit();
         }
 
-
         spec = host.newTabSpec("tab3");
         spec.setContent(R.id.tab_content3);
-        spec.setIndicator("랜덤");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_roulette, null));
         host.addTab(spec);
 
         spec = host.newTabSpec("tab4");
         spec.setContent(R.id.tab_content4);
-        spec.setIndicator("리뷰");
+        spec.setIndicator(null, ResourcesCompat.getDrawable(getResources(), R.drawable.tab_pen, null));
         host.addTab(spec);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        reviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("리뷰를 등록하시겠습니까?")
+                        .setPositiveButton("등록", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "리뷰가 성공적으로 등록되었습니다.", Toast.LENGTH_SHORT).show();
+                                searchText.setText("");
+                                reviewText.setText("");
+                            }
+                        })
+                        .setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(MainActivity.this, "취소 버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                dialog.create();
+                dialog.show();
+            }
+        });
+
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.home);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.user);
 
         SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
-        searchView = findViewById(R.id.menu_search);
+
         searchView.onActionViewExpanded();
         searchView.setQueryHint("이름을 검색하시오");
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
@@ -116,9 +144,13 @@ public class MainActivity extends AppCompatActivity {
 
         switch (view.getId()) {
             case R.id.button_map:
+                mapButton.setBackgroundColor(0xffD95550);
+                sortButton.setBackgroundColor(0xffaaaaaa);
                 fr = new Fragment2_1();
                 break;
             case R.id.button_sort:
+                mapButton.setBackgroundColor(0xffaaaaaa);
+                sortButton.setBackgroundColor(0xffD95550);
                 fr = new Fragment2_2();
                 break;
         }
@@ -172,7 +204,7 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onclick (View view){
+    public void onclick(View view) {
         int id = view.getId();
         switch (id) {
             case R.id.my_info: {
