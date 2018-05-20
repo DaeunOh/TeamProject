4909856d4b459ae7f08fc43470dfe76616fae457
LAPTOP <nyk0520@gmail.com>
2018-05-21 01:02:26 +0900

    searchview(수정-입력했을 때와 처음 화면이 떴을 때에 키보드를 닫아둠) 슬라이딩메뉴를 구현해봄 클릭했을 때 에러가 뜸.
    에러 수정할 예정.. 슬라이딩 메뉴 안에 listview를 통해 메뉴를 구현할 에정임
    또한 다른 탭에선 툴바가 아닌 기존의 액션바를 사용하도록 구현할 예정임.

--- app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
+++ app/src/main/java/com/example/bbbb/teamproject/MainActivity.java
@@ -7,21 +7,28 @@ import android.app.FragmentTransaction;
 import android.app.SearchManager;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
-
-    android.support.v7.widget.SearchView searchView;
+    private Toolbar toolbar;
+    private DrawerLayout dlDrawer;
+    private ActionBarDrawerToggle dtToggle;
+    private android.support.v7.widget.SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,12 +83,12 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.home);
 
-
         SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
         searchView = findViewById(R.id.menu_search);
         searchView.onActionViewExpanded();
         searchView.setQueryHint("이름을 검색하시오");
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.clearFocus();
         searchView.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
             //검색 버튼을 클릭했을 때 동작하는 이벤트 query:입력된 검색어
             @Override
@@ -90,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SearchActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
                 intent.putExtra("query", query);
-
+                searchView.clearFocus();
 
                 return false;
             }
@@ -130,16 +137,51 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+//    @Override
+//    public void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//
+//        dtToggle.syncState();
+//    }
+//
+//    @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//        dtToggle.onConfigurationChanged(newConfig);
+//    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
             case android.R.id.home: {
+                dlDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+                dtToggle = new ActionBarDrawerToggle(this, dlDrawer, R.string.app_name, R.string.app_name);
+                dlDrawer.setDrawerListener(dtToggle);
 
+                dlDrawer.openDrawer(Gravity.START);
+                return true;
+            }
+            case R.id.lang: {
+                return true;
+            }
+            case R.id.colorch: {
                 return true;
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onclick (View view){
+        int id = view.getId();
+        switch (id) {
+            case R.id.my_info: {
+                dlDrawer.closeDrawer(Gravity.START);
+            }
+            case R.id.my_review: {
+                dlDrawer.closeDrawer(Gravity.START);
+            }
+        }
     }
 
 
