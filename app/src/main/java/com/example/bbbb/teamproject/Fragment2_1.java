3c28654bcb7c3f578908d763886a00e482604707
DaeunOh <d30902430@gmail.com>
2018-05-22 05:52:46 +0900

    구글맵 띄우기 - 마커 샘플 추가

--- app/src/main/java/com/example/bbbb/teamproject/Fragment2_1.java
+++ app/src/main/java/com/example/bbbb/teamproject/Fragment2_1.java
@@ -1,19 +1,128 @@
 package com.example.bbbb.teamproject;
 
 import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by SUNSOOMIN on 2018-05-19.
  */
 
-public class Fragment2_1 extends Fragment {
+public class Fragment2_1 extends Fragment implements OnMapReadyCallback {
+
+    private MapView mapView = null;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
-        return inflater.inflate(R.layout.fragment2_1,container,false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+
+        View layout = inflater.inflate(R.layout.fragment2_1, container, false);
+
+        mapView = (MapView) layout.findViewById(R.id.map);
+        mapView.getMapAsync(this);
+
+        return layout;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //액티비티가 처음 생성될 때 실행되는 함수
+        if(mapView != null) {
+            mapView.onCreate(savedInstanceState);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        LatLng ajouUniv = new LatLng(37.277516, 127.043829);
+
+        LatLng 천애부 = new LatLng(37.279168, 127.043357);
+        LatLng 아롤도그 = new LatLng(37.279036, 127.043540);
+        LatLng 맘스터치 = new LatLng(37.279015, 127.043503);
+
+        googleMap.addMarker(new MarkerOptions().position(천애부).title("천애부").alpha(0.5f));
+        googleMap.addMarker(new MarkerOptions().position(아롤도그).title("아롤도그").alpha(0.5f).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+        googleMap.addMarker(new MarkerOptions().position(맘스터치).title("맘스터치").alpha(0.5f).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ajouUniv, 16));
+
+        // 정보 눌렀을 때
+        googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+            }
+        });
+
+        // 마커 눌렀을 때
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                return false;
+            }
+        });
+
+    }
 }
